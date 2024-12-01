b9b7f5c6461336043cd32508bc7c5f6ebdc88255
chimp1999 <chinmaypati4133@gmail.com>
2024-12-01 18:12:15 +0530

    2024 Day 1 solutions added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,9 +8,9 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         // Year 2023
-        Runner2023.execute();
+//        Runner2023.execute();
 
         // Year 2024
-//        Runner2024.execute();
+        Runner2024.execute();
     }
 }
---
+++ src/main/java/org/example/Year2024/day1/Day1.java
@@ -0,0 +1,63 @@
+package org.example.Year2024.day1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Day1 {
+    private static List<Long> list1 = new ArrayList<>(), list2 = new ArrayList<>();
+
+    public static void run(int part) throws FileNotFoundException {
+        long startTime = System.currentTimeMillis();
+        Scanner sc = new Scanner(new File("src/main/resources/Input Data/Year2024/inputDay1.txt"));
+
+        while(sc.hasNextLine()){
+            String[] input = sc.nextLine().split(" {3}");
+            list1.add(Long.parseLong(input[0]));
+            list2.add(Long.parseLong(input[1]));
+        }
+
+        long res = (part == 1) ? calculateTotalDistance() : calculateSimilarityScore();
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Day 1 Part "+part+" : "+ res+"     Execution Time : "+(endTime-startTime)+"ms");
+    }
+
+    private static long calculateTotalDistance() {
+        Collections.sort(list1);
+        Collections.sort(list2);
+
+        long totalDistance = 0L;
+
+        for(int i=0; i<list1.size(); i++){
+            totalDistance += Math.abs(list1.get(i) - list2.get(i));
+        }
+
+        return totalDistance;
+    }
+
+    private static long calculateSimilarityScore() {
+        // create freq map for both then iterate the map and calculate score.
+        Map<Long, Long> map1 = generateFrequencyMap(list1);
+        Map<Long, Long> map2 = generateFrequencyMap(list2);
+
+        long similarityScore = 0L;
+
+        for(Map.Entry<Long, Long> entry : map1.entrySet()){
+            similarityScore += entry.getKey() * map2.getOrDefault(entry.getKey(), 0L) * entry.getValue();
+        }
+
+        return similarityScore;
+    }
+
+    private static Map<Long, Long> generateFrequencyMap(List<Long> inputList){
+        Map<Long, Long> frequencyMap = new HashMap<>();
+
+        for(Long value : inputList){
+            frequencyMap.put(value, frequencyMap.getOrDefault(value, 0L)+1L);
+        }
+
+        return frequencyMap;
+    }
+}
